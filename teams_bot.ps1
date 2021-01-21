35afaff6e5276e7d26a1bbeeb3c73c1103de4229
tgorham1980 <74372515+tgorham1980@users.noreply.github.com>
2021-01-20 20:20:37 -0500

    Update teams_bot.ps1

--- teams_bot.ps1
+++ teams_bot.ps1
@@ -1,3 +1,3 @@
 # PowerShell PS1 file to test GitHub Chat bot and Teams channel.
 # Edit 1 after reinstalling GitHub bot into subchannel.
-# Edit 2 post subscription within channel
+# Edit 2 post subscription within sub channel
